1db642c9155e8e4ea08556871be4a2befc742555
Loci <reg-artur@mail.ru>
2020-06-09 15:28:27 +0300

    Если нет табеля - выводим данные графика

--- controllers/employee.js
+++ controllers/employee.js
@@ -648,6 +648,202 @@ module.exports.getEmployeeWorkSchedulesDataForPeriod = async function (req, res)
     }
 }
 
+const getUserWorkSchedulesDataForMonth = async function (base_pk, pk, year, month) {
+    const client = await db.client()
+
+    try {
+        await client.query('BEGIN')
+
+        const begin_date = new Date(year, month, 1, 0, 0, 0, 0)
+        const end_date = new Date(year, month +1, 1, 0, 0, 0, 0)
+
+        const sqlCreate = `
+                    Create Temp Table Prioritet AS
+                    Select DISTINCT
+                           personal_work_schedules_data.employee_id_1c,   
+                           personal_work_schedules_data.base_pk as base_pk,
+                           personal_work_schedules_data.work_date
+                    From personal_work_schedules_data
+                         inner join employee on (personal_work_schedules_data.employee_id_1c = employee.pk
+                                                and personal_work_schedules_data.base_pk = employee.base_pk)
+                                    inner join users on (employee.user_id_1c = users.id_1c
+                                                        and employee.base_pk = users.base_pk)   
+                    where 
+                               personal_work_schedules_data.work_date >= $1
+                               and personal_work_schedules_data.work_date < $2
+                               and personal_work_schedules_data.base_pk = $3
+                               and users.pk = $4;`
+        client.query(sqlCreate, [begin_date,
+            end_date,
+            base_pk,
+            pk])
+        const sqlSel = `
+                    Select 
+                        * 
+                    FROM
+                        (
+                            (Select
+                                data.work_date,
+                                data.work_hour,
+                                data.time_name,
+                                data.time_kod,
+                                data.employee_id_1c, 
+                                q
+                            FROM     
+                                (Select
+                                general_work_schedules_data.work_date,
+                                general_work_schedules_data.work_hour,
+                                types_of_time.time_name,
+                                types_of_time.time_kod,
+                                employee_work_schedules.employee_id_1c, 
+                                1 as q
+                                From 
+                                     employee_work_schedules
+                                     inner join employee on (employee_work_schedules.employee_id_1c = employee.pk
+                                                            and employee_work_schedules.base_pk = employee.base_pk)
+                                                inner join users on (employee.user_id_1c = users.id_1c
+                                                                    and employee.base_pk = users.base_pk)                         
+                                     inner join general_work_schedules_data on 
+                                           (employee_work_schedules.work_schedules_id_1c = general_work_schedules_data.work_schedule_id_1c
+                                           and employee_work_schedules.base_pk = general_work_schedules_data.base_pk
+                                           and employee_work_schedules.date_from <= general_work_schedules_data.work_date
+                                           and employee_work_schedules.date_to > general_work_schedules_data.work_date
+                                           and general_work_schedules_data.work_date >= $1
+                                           and general_work_schedules_data.work_date < $2
+                                           )
+                                               inner join types_of_time on 
+                                                    (general_work_schedules_data.base_pk = types_of_time.base_pk
+                                                    and general_work_schedules_data.types_of_time_id_1c = types_of_time.id_1c)
+                                Where
+                                      ((employee_work_schedules.date_from >= $3
+                                     and employee_work_schedules.date_from < $4)
+                                     or                         
+                                     (     employee_work_schedules.date_to >= $5
+                                     and employee_work_schedules.date_to < $6)
+                                     or
+                                     (employee_work_schedules.date_from <= $7
+                                     and employee_work_schedules.date_to > $8))
+                                     and 
+                                     users.pk = $9
+                                     and employee_work_schedules.base_pk = $10
+                                ) as data
+                                left join Prioritet on (data.employee_id_1c = Prioritet.employee_id_1c
+                                                         and data.work_date = Prioritet.work_date)
+                                Where 
+                                    Prioritet.base_pk is NULL                           
+                            )
+                            UNION all
+                            (Select
+                            personal_work_schedules_data.work_date,
+                            personal_work_schedules_data.work_hour,
+                            types_of_time.time_name,
+                            types_of_time.time_kod,
+                            employee_work_schedules.employee_id_1c,
+                            2
+                            From
+                                 employee_work_schedules
+                                 inner join employee on (employee_work_schedules.employee_id_1c = employee.pk
+                                                        and employee_work_schedules.base_pk = employee.base_pk)
+                                            inner join users on (employee.user_id_1c = users.id_1c
+                                                                and employee.base_pk = users.base_pk)
+                                 inner join personal_work_schedules_data on
+                                       (personal_work_schedules_data.employee_id_1c = employee_work_schedules.employee_id_1c
+                                       and employee_work_schedules.base_pk = personal_work_schedules_data.base_pk
+                                       and employee_work_schedules.date_from <= personal_work_schedules_data.work_date
+                                       and employee_work_schedules.date_to > personal_work_schedules_data.work_date
+                                       and personal_work_schedules_data.work_date >= $11
+                                       and personal_work_schedules_data.work_date < $12
+                                       )
+                                           inner join types_of_time on
+                                                (personal_work_schedules_data.base_pk = types_of_time.base_pk
+                                                and personal_work_schedules_data.types_of_time_id_1c = types_of_time.id_1c)
+                                       left join Prioritet on (personal_work_schedules_data.employee_id_1c = Prioritet.employee_id_1c
+                                                                    and personal_work_schedules_data.base_pk = Prioritet.base_pk
+                                                                    and personal_work_schedules_data.work_date = Prioritet.work_date)                                        
+                            Where
+                                 (employee_work_schedules.date_from >= $13
+                                 and employee_work_schedules.date_from < $14)
+                                 or
+                                 (     employee_work_schedules.date_to >= $15
+                                 and employee_work_schedules.date_to < $16)
+                                 or
+                                 (     employee_work_schedules.date_from <= $17
+                                 and employee_work_schedules.date_to > $18)
+                                 and
+                                 users.pk = $19
+                                 and
+                                 employee_work_schedules.base_pk = $20
+                            )
+                        )  as endData
+                    Order by
+                        employee_id_1c,
+                        work_date`
+        const {rows} = await client.query(sqlSel,
+            [begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                pk,
+                base_pk,
+                begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                begin_date,
+                end_date,
+                pk,
+                base_pk]);
+
+        const sqlDel = 'DROP Table Prioritet'
+        await client.query(sqlDel)
+
+        client.query('COMMIT')
+        client.release()
+
+        const result = []
+        let currentEmployee = null
+        let currentMonth = null
+        let employeeBlock = null
+        let monthBlock = null
+        for (item of rows) {
+            if (item.employee_id_1c !== currentEmployee) {
+                currentMonth = null
+                employeeBlock = {
+                    employee: item.employee_id_1c,
+                    months: []
+                }
+                result.push(employeeBlock)
+                currentEmployee = item.employee_id_1c
+                currentMonth = null
+                monthBlock = null
+            }
+            if (item.work_date.getMonth() !== currentMonth) {
+                monthBlock = {
+                    month: item.work_date.getMonth(),
+                    days: []
+                }
+                employeeBlock.months.push(monthBlock)
+                currentMonth = item.work_date.getMonth()
+            }
+
+            monthBlock.days.push(item)
+        }
+
+        return result
+    } catch (e) {
+        errorHandler([base_pk, pk, year, month], e)
+        await client.query('ROOLBACK')
+        client.release()
+        throw e
+    }
+}
+
 module.exports.updatEemployeeTabel = async function (req, res) {
     const client = await db.client()
 
@@ -698,7 +894,7 @@ module.exports.updatEemployeeTabel = async function (req, res) {
 
 module.exports.getEmployeeTabel = async function (req, res) {
     try {
-        const pk = req.user.pk
+        const {pk, base_pk} = req.user
         const month = req.query.month
         const sql = ` Select
                         employee_tabel.tabel_data
@@ -712,11 +908,54 @@ module.exports.getEmployeeTabel = async function (req, res) {
                         users.pk = $1
                         and 
                         employee_tabel.tabel_month = $2`
-
-
+        
         const {rows} = await db.query(sql, [pk, month])
+        
+        if (rows.length) {
+            res.status(200).json(rows)
+        } else {
+            const monthDate = new Date(month)
+            console.log(base_pk, pk, monthDate.getFullYear(), monthDate.getMonth())
+            const schedulesData = await getUserWorkSchedulesDataForMonth(base_pk, pk, monthDate.getFullYear(), monthDate.getMonth())
 
-        res.status(200).json(rows)
+            const resultArray = []
+            for (let person of schedulesData) {
+                const resultObject = {}
+                resultObject.Сотрудник = schedulesData.employee
+                resultObject.ТабНом = ''
+                let monthHours = 0
+                let monthDays = 0
+                let dayOfMonth = {}
+                const days = person.months[0].days
+                for (let day of days) {
+                    const dayNumber = new Date(day.work_date).getDate()
+
+                    if (day.work_hour > 0 ){dayOfMonth['День' + dayNumber] = 1}
+
+                    resultObject['Символ' + dayNumber] = resultObject['Символ' + dayNumber]
+                        ? resultObject['Символ' + dayNumber] + '/' + day.time_kod
+                        : day.time_kod
+                    if (resultObject['ДополнительноеЗначение' + dayNumber]) {
+                        resultObject['ДополнительноеЗначение' + dayNumber] = resultObject['ДополнительноеЗначение' + dayNumber]
+                            + day.work_hour
+                        monthHours = monthHours + day.work_hour
+                    } else {
+                        resultObject['ДополнительноеЗначение' + dayNumber] = day.work_hour
+                        monthHours = monthHours + day.work_hour
+                    }
+                }
+
+                monthDays = Object.keys(dayOfMonth).length
+
+                resultObject.ДниЗаМесяц = monthDays
+                resultObject.ЧасыЗаМесяц = monthHours
+                resultObject.ОтклоненияПоСотруднику = []
+
+                resultArray.push({tabel_data: JSON.stringify(resultObject)})
+            }
+            console.log(resultArray)
+            res.status(200).json(resultArray)
+        }   
     } catch (e) {
         errorHandler(res, e)
     }
